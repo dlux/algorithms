cafc3375ae1ad07855bf9393cb1a6bb6a4d737fc
Luz Cazares <luz.cazares@intel.com>
2018-01-22 18:27:40 -0800

    Use pbr to install requirements
    To avoid pip installation and handling of dependencies/
    manual installation. Use pbr library.

--- setup.py
+++ setup.py
@@ -1,49 +1,13 @@
-# Setuptools is used to facilitate packaging
-# Python projects.
+import setuptools
 
-from setuptools import setup, find_packages
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-
-# Install requirements from requirements.txt
-requirements=[]
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(
-    # Package details
-    name = 'dluxAlgorithms',
-    version = '1.0.0',
-    packages = find_packages(),
-
-    #metadata for upload to PyPI
-    description = 'Packaging a python project which will contain algorithm code exercises',
-    author = 'Luz Cazares',
-    author_email = 'tmp@dlux.com',
-    url = 'https://github.com/dlux/algorithms',
-    test_suite='tests',
-    keywords = ['dlux', 'algorithms', 'mypackage'],
-    install_requires=requirements,
-
-    # Additional metadata
-    classifier = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Quality Engineers',
-        'Intended Audience :: Information Technology',
-        'License :: Free For Educational Use',
-        'Operating System :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        ],
-    entry_points = {
-         'console_scripts':[
-             'dlux_alg = src.cmd.main:main',
-             ],
-         'dlux_alg.cm':[
-             'union-find = src.cmd.client_union_find:UNIONFIND',
-             ]	
-         }
-)
-
+setuptools.setup(
+    setup_requires=['pbr>=2.0.0'],
+    pbr=True)
